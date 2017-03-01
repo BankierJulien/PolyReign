965b924cf9424b777d7960c5b3aea82e50b87ca2
BankierJulien <julienbankiernyc@gmail.com>
2017-02-28 22:35:02 -0500

    Initial Commit

---
+++ PolyReign/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PolyReign
+//
+//  Created by Julien Bankier on 2/28/17.
+//  Copyright © 2017 Julien Bankier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
